29a9573a7ba717bb255cb0544753fcccbaf48339
Clashsoft <clashsoft@hotmail.com>
2014-08-10 00:05:00 +0200

    Update

--- src/main/java/clashsoft/mods/moredimensions/world/teleporters/TeleporterHeaven.java
+++ src/main/java/clashsoft/mods/moredimensions/world/teleporters/TeleporterHeaven.java
@@ -3,7 +3,6 @@ package clashsoft.mods.moredimensions.world.teleporters;
 import clashsoft.cslib.minecraft.world.CustomTeleporter;
 import clashsoft.mods.moredimensions.lib.Heaven;
 
-import net.minecraft.block.Block;
 import net.minecraft.world.WorldServer;
 
 public class TeleporterHeaven extends CustomTeleporter
@@ -11,17 +10,7 @@ public class TeleporterHeaven extends CustomTeleporter
 	public TeleporterHeaven(WorldServer world)
 	{
 		super(world);
-	}
-
-	@Override
-	public Block getPortalBlock()
-	{
-		return Heaven.portal;
-	}
-
-	@Override
-	public Block getFrameBlock()
-	{
-		return Heaven.portalFrame;
+		this.portalBlock = Heaven.portal;
+		this.frameBlock = Heaven.portalFrame;
 	}
 }
--- src/main/java/clashsoft/mods/moredimensions/world/teleporters/TeleporterPOC.java
+++ src/main/java/clashsoft/mods/moredimensions/world/teleporters/TeleporterPOC.java
@@ -3,7 +3,6 @@ package clashsoft.mods.moredimensions.world.teleporters;
 import clashsoft.cslib.minecraft.world.CustomTeleporter;
 import clashsoft.mods.moredimensions.lib.ParadiseOfChaos;
 
-import net.minecraft.block.Block;
 import net.minecraft.world.WorldServer;
 
 public class TeleporterPOC extends CustomTeleporter
@@ -11,17 +10,7 @@ public class TeleporterPOC extends CustomTeleporter
 	public TeleporterPOC(WorldServer world)
 	{
 		super(world);
-	}
-
-	@Override
-	public Block getPortalBlock()
-	{
-		return ParadiseOfChaos.portal;
-	}
-
-	@Override
-	public Block getFrameBlock()
-	{
-		return ParadiseOfChaos.portalFrame;
+		this.portalBlock = ParadiseOfChaos.portal;
+		this.frameBlock = ParadiseOfChaos.portalFrame;
 	}
 }
