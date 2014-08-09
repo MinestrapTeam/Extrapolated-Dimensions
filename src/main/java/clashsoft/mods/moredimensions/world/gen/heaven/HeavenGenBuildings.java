91ae3cc121a661bf1ad75f87d387ab1d5f55d6d0
Clashsoft <clashsoft@hotmail.com>
2014-08-09 20:28:05 +0200

    Removed Building1 generator

--- src/main/java/clashsoft/mods/moredimensions/world/gen/heaven/HeavenGenBuildings.java
+++ src/main/java/clashsoft/mods/moredimensions/world/gen/heaven/HeavenGenBuildings.java
@@ -2,30 +2,20 @@ package clashsoft.mods.moredimensions.world.gen.heaven;
 
 import java.util.Random;
 
-import clashsoft.cslib.minecraft.world.CSWorld;
-import clashsoft.mods.moredimensions.lib.Heaven;
+import clashsoft.cslib.minecraft.world.gen.CustomWorldGen;
 
-import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
-import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class HeavenGenBuildings
 {
-	public static HeavenGenBuildings	instance	= new HeavenGenBuildings();
-	
 	/**
 	 * The WorldGen for a little hut
 	 * 
 	 * @author Clashsoft
 	 */
-	public static class HeavenGenBuilding1 extends WorldGenerator
+	public static class SimpleHut extends CustomWorldGen
 	{
-		public HeavenGenBuilding1()
-		{
-			
-		}
-		
-		public HeavenGenBuilding1(boolean flag)
+		public SimpleHut(boolean flag)
 		{
 			super(flag);
 		}
@@ -33,27 +23,6 @@ public class HeavenGenBuildings
 		@Override
 		public boolean generate(World world, Random rand, int x, int y, int z)
 		{
-			return this.generateN(world, rand, x, y, z);
-		}
-		
-		public boolean generateN(World world, Random rand, int x, int y, int z)
-		{
-			// Heavencobble frame
-			CSWorld.setFrame(world, x, y, z, x + 6, y + 4, z + 6, Heaven.stoneBlocks, 1);
-			// Walls
-			CSWorld.setCube(world, x, y + 1, z + 1, x, y + 3, z + 5, Heaven.plankBlocks, 0);
-			CSWorld.setCube(world, x + 1, y + 1, z, x + 5, y + 3, z, Heaven.plankBlocks, 0);
-			CSWorld.setCube(world, x + 6, y + 1, z + 1, x + 6, y + 3, z + 5, Heaven.plankBlocks, 0);
-			CSWorld.setCube(world, x + 1, y + 1, z + 6, x + 5, y + 3, z + 6, Heaven.plankBlocks, 0);
-			// Floor
-			CSWorld.setCube(world, x + 1, y, z + 1, x + 5, y, z + 5, Heaven.logBlocks, 0);
-			// Door
-			CSWorld.setCube(world, x + 3, y + 1, z, x + 3, y + 2, z, Blocks.air, 0);
-			// Windows
-			CSWorld.setBlock(world, x + 3, y + 2, z + 6, Blocks.glass_pane, 0);
-			CSWorld.setBlock(world, x + 3, y, z, Blocks.glass_pane, 0);
-			CSWorld.setBlock(world, x, y, z + 3, Blocks.glass_pane, 0);
-			CSWorld.setBlock(world, x + 6, y, z + 3, Blocks.glass_pane, 0);
 			return false;
 		}
 	}
