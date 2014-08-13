6b30cd1a65be2075f76660ae7fb27d32e155f17c
Clashsoft <clashsoft@hotmail.com>
2014-08-13 17:04:50 +0200

    Portals

--- src/main/java/minestrapteam/extradims/block/aerius/BlockAeriusPortal.java
+++ src/main/java/minestrapteam/extradims/block/aerius/BlockAeriusPortal.java
@@ -6,6 +6,7 @@ import minestrapteam.extradims.world.teleporters.AeriusTeleporter;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.world.Teleporter;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
 public class BlockAeriusPortal extends BlockCustomPortal
@@ -20,5 +21,10 @@ public class BlockAeriusPortal extends BlockCustomPortal
 	public Teleporter createTeleporter(WorldServer world)
 	{
 		return new AeriusTeleporter(world);
+	}
+
+	@Override
+	public void spawnParticle(World world, double x, double y, double z, double vX, double vY, double vZ)
+	{
 	}
 }
--- src/main/java/minestrapteam/extradims/block/poc/BlockPOCPortal.java
+++ src/main/java/minestrapteam/extradims/block/poc/BlockPOCPortal.java
@@ -6,6 +6,7 @@ import minestrapteam.extradims.lib.WorldManager;
 import minestrapteam.extradims.world.teleporters.TeleporterPOC;
 
 import net.minecraft.world.Teleporter;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
 public class BlockPOCPortal extends BlockCustomPortal
@@ -20,5 +21,10 @@ public class BlockPOCPortal extends BlockCustomPortal
 	public Teleporter createTeleporter(WorldServer world)
 	{
 		return new TeleporterPOC(world);
+	}
+
+	@Override
+	public void spawnParticle(World world, double x, double y, double z, double vX, double vY, double vZ)
+	{
 	}
 }
