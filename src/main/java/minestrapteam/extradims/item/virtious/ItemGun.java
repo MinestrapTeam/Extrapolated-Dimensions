75cd11fe0b29f61bc74e77a3866294e840e2dae2
Clashsoft <clashsoft@hotmail.com>
2016-04-30 15:56:38 +0200

    Clean up ItemGun and ItemStickBomb
    - Clean up the `ItemGun` and `ItemStickBomb` classes to only spawn
    entities server-side.

--- src/main/java/minestrapteam/extradims/item/virtious/ItemGun.java
+++ src/main/java/minestrapteam/extradims/item/virtious/ItemGun.java
@@ -1,7 +1,6 @@
 package minestrapteam.extradims.item.virtious;
 
 import minestrapteam.extradims.entity.EntityLaser;
-
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -9,41 +8,44 @@ import net.minecraft.world.World;
 
 public class ItemGun extends Item
 {
-	private int			firetick;
-	
-	protected int		damage;
-	protected int		cooldown;
-	protected String	sound;
-	
+	private int firetick;
+
+	protected int    damage;
+	protected int    cooldown;
+	protected String sound;
+
 	public ItemGun(int uses, int damage, int cooldown, String sound)
 	{
 		this.damage = damage;
 		this.cooldown = cooldown;
-		
+
 		this.setMaxStackSize(1);
 		this.setMaxDamage(uses);
 	}
-	
+
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
 	{
 		player.setItemInUse(stack, 2000);
 		return stack;
 	}
-	
+
 	@Override
 	public void onPlayerStoppedUsing(ItemStack stack, World world, EntityPlayer player, int ticks)
 	{
-		world.spawnEntityInWorld(new EntityLaser(world, player, this.damage));
+		if (!world.isRemote)
+		{
+			world.spawnEntityInWorld(new EntityLaser(world, player, this.damage));
+		}
 		stack.damageItem(this.damage, player);
 	}
-	
+
 	@Override
 	public int getMaxItemUseDuration(ItemStack stack)
 	{
 		return this.cooldown;
 	}
-	
+
 	@Override
 	public boolean isFull3D()
 	{
--- src/main/java/minestrapteam/extradims/item/virtious/ItemStickyBomb.java
+++ src/main/java/minestrapteam/extradims/item/virtious/ItemStickyBomb.java
@@ -1,7 +1,6 @@
 package minestrapteam.extradims.item.virtious;
 
 import minestrapteam.extradims.entity.item.EntityStickyBomb;
-
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -13,16 +12,18 @@ public class ItemStickyBomb extends Item
 	@Override
 	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
 	{
-		EntityStickyBomb bomb = new EntityStickyBomb(world, x + 0.5D, y + 1D, z + 0.5D, player);
-		world.spawnEntityInWorld(bomb);
-		
+		if (!world.isRemote)
+		{
+			world.spawnEntityInWorld(new EntityStickyBomb(world, x + 0.5D, y + 1D, z + 0.5D, player));
+		}
+
 		if (!player.capabilities.isCreativeMode)
 		{
 			stack.stackSize--;
 		}
 		return true;
 	}
-	
+
 	@Override
 	public void registerIcons(IIconRegister iconRegister)
 	{
